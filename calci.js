444629fa61334cc0c5af36055fce32b94d073450
sahilkhan18 <127738034+sahilkhan18@users.noreply.github.com>
2023-03-26 13:32:53 +0530

    Add files via upload

---
+++ calci.js
@@ -0,0 +1,28 @@
+let output = document.getElementById("output");
+
+function display(num)
+{
+    output.value += num;
+}
+
+function Calculate()
+{
+    try
+    {
+        output.value = eval(output.value);
+    }
+    catch(err)
+    {
+        alert("invalid input");
+    }
+}
+
+function Clear()
+{
+    output.value = "";
+}
+
+function del()
+{
+    output.value = output.value.slice(0,-1);
+}
